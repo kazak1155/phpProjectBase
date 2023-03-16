77bd1f9a8a021818ea487adc69a1a3ba8a2a5f0d
AndreyV <kazakmt4@gmail.com>
2023-03-16 19:05:16 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+$test = new \App\classes\Test();
---
+++ src/classes/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\classes;
+
+class Test
+{
+    public function __construct()
+    {
+        echo 'TEST';
+    }
+}
